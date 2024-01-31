d9df9358bd2ce70c2a129c6bbec1ce35e0b7e490
ninbura <gabriel@ninbura.com>
2024-01-30 21:10:21 -0700

    Changed which PowerShell profile gets edited by setup.ps1, update file encoding for pretty linebreaks

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -12,22 +12,22 @@ function Startup() {
 function VerifyOrCreateProfile() {
   write-host "Verifying or creating profile... " -NoNewLine
 
-  if (!(test-path $profile.AllUsersAllHosts)) {
-    New-Item -Path $profile.AllUsersAllHosts -ItemType File
+  if (!(test-path $profile.CurrentUserAllHosts)) {
+    New-Item -Path $profile.CurrentUserAllHosts -ItemType File
   }
 
   write-host "[OK]`n" -ForegroundColor Green
 }
 
 function AddArcScriptsToProfile() {
-  $profileContents = Get-Content -Path $profile.AllUsersAllHosts
-  $finalIndex = $profileContents.Count - 1
+  $profileContents = Get-Content -Path $profile.CurrentUserAllHosts -Raw
   $additionalContents = @(
     "# Arc Scripts",
     "Set-Alias -Name arc-backup -Value `"$PSScriptRoot/arc-backup.ps1`"",
     "Set-Alias -Name arc-sync -Value `"$PSScriptRoot/arc-sync.ps1`""
   )
-  $profileAlreadyContainsArcScripts = $profileContents | Where-Object { $_ -like "*arc-backup*" -or $_ -like "*arc-sync*" }
+
+  $profileAlreadyContainsArcScripts = $profileContents -match "arc-backup.ps1" -or $profileContents -match "arc-sync.ps1"
 
   if ($profileAlreadyContainsArcScripts) {
     write-host "Arc scripts already exist in profile, skipping...`n" -ForegroundColor Yellow
@@ -37,14 +37,16 @@ function AddArcScriptsToProfile() {
 
   write-host "Adding arc scripts to profile... " -NoNewLine
 
-  if($profileContents.Length -eq 0 -or $profileContents[$finalindex] -match "[\r\n]") {
-    $profileContents += $additionalContents
+  if($profileContents.Length -eq 0) {
+    $profileContents += $additionalContents.Join("`n")
   } else {
-    $profileContents += "`n" + $additionalContents
+    if($profileContents -notmatch "\s[\r\n]$") { $profileContents += "`n" }
+
+    $profileContents += $($additionalContents -join "`n")
   }
 
   try{
-    Set-Content -Path $profile.AllUsersAllHosts -Value $profileContents -Force
+    Set-Content -Path $profile.CurrentUserAllHosts -Value $profileContents -NoNewLine -Force
   } catch {
     write-host "[FAIL]" -ForegroundColor Red
     write-host "There was an error adding arc scripts to profile, please check your configuration file and try again." -ForegroundColor Red
@@ -56,7 +58,7 @@ function AddArcScriptsToProfile() {
 }
 
 function Conclude() {
-  . $profile.AllUsersAllHosts
+  . $profile.CurrentUserAllHosts
   
   write-host "arc-setup completed successfully." -ForegroundColor Green
 }
